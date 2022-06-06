545d18aa3294c40353e5f3016a9167386a592082
YoussefBedhief <69478528+YoussefBedhief@users.noreply.github.com>
2022-06-06 01:53:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   mode: 'jit',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}',],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
+],
   theme: {
     extend: {},
   },
